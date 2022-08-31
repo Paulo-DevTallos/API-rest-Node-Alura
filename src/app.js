3e71d12231fc11249f95373e67e4497cf6a631f8
Paulo Sergio <pauloserg861@gmail.com>
2022-08-31 15:21:45 -0300

    implementando metodos de put e delete

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const books = [
     { id: 2, titulo: "O Hobbit" },
 ]
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.status(200).send('express renderizou!')
 })
@@ -14,5 +17,35 @@ app.get('/', (req, res) => {
 app.get('/livros', (req, res) => {
     res.status(200).json(books)
 })
+
+app.get('/livros/:id', (req, res) => {
+    let index = buscarLivros(req.params.id)
+    res.json(books[index])
+})
+
+//post sempre tem que pegar o que vem no body
+app.post('/livros', (req, res) => {
+    books.push(req.body)
+    res.status(201).send('livro cadastrado com sucesso!')
+})
+
+app.put('/livros/:id', (req, res) => {
+    let index = buscarLivros(req.params.id)
+    books[index].titulo = req.body.titulo // o index que eu pesquisar do array será atualizado de acordo com o que vier no corpo da requisição
+
+    res.json(books)
+})
+
+app.delete('/livros/:id', (req, res) => {
+    let { id } = req.params
+    let index = buscarLivros(id)
+    books.splice(index, 1) // qual a posição do item que será excluido
+    res.send(`livro ${id} removido da lista`)
+})
+
+function buscarLivros(id) {
+    return books.findIndex(book => book.id === id)
+}
+
 
 module.exports = app
