804b26f7f08e724bb9248296652bf60606d55ed4
Paulo Sergio <pauloserg861@gmail.com>
2022-08-31 14:08:44 -0300

    criando readme / objeto de rotas com node puro / instalando nodemon

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 curso de rest API com NodeJS / Express / MongoDB
 
 *** comando para rodar servidor com node.NodeJS: node . | node server.NodeJS
+*** nomeando server running com nodemon: "script": "nodemon"   =   comando: npm start
--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const http = require('http')
 //import { createServer } from 'http' - ES6
 const port = 3000 || process.env.port
 
+
+const routes = {
+    '/': 'Curso de NodeJS',
+    '/livros': 'Entrei na página de livros',
+    '/autores': 'Listagem de autores',
+    '/editoras': 'Página de editoras'
+}
+
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'content-type': 'text/plain'})
-    res.end('Olá mundo!')
+    res.end(routes[req.url]) //req.url faz uma leitura do recurso da rota 
 })
 
 server.listen(port, () => {
