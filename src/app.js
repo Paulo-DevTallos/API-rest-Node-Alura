8dd50b6abffa1221cb5fb2d04751a11b7c625e92
Paulo Sergio <pauloserg861@gmail.com>
2022-09-01 09:56:03 -0300

    criando metodos de update / delete e implementando as rotas

--- src/app.js
+++ src/app.js
@@ -4,45 +4,17 @@ const db = require('./db')
 
 const app = express()
 
-
-const books = [
-    { id: 1, titulo: "O Senhor dos anéis" },
-    { id: 2, titulo: "O Hobbit" },
-]
-
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 db.dbConnect()
 
+app.get('/', (req, res) => {res.send('rotas inicial')})
 app.get('/livros', booksController.getAll)
 app.get('/livros/:id', booksController.getById)
 app.post('/livros', booksController.post)
-
-/*app.get('/livros', (req, res) => {
-    res.status(200).json(books)
-})*/
-
-
-//post sempre tem que pegar o que vem no body
-
-app.put('/livros/:id', (req, res) => {
-    let index = buscarLivros(req.params.id)
-    books[index].titulo = req.body.titulo // o index que eu pesquisar do array será atualizado de acordo com o que vier no corpo da requisição
-
-    res.json(books)
-})
-
-app.delete('/livros/:id', (req, res) => {
-    let { id } = req.params
-    let index = buscarLivros(id)
-    books.splice(index, 1) // qual a posição do item que será excluido
-    res.send(`livro ${id} removido da lista`)
-})
-
-function buscarLivros(id) {
-    return books.findIndex(book => book.id === id)
-}
+app.put('/livros/:id', booksController.updateBook)
+app.delete('/livros/:id', booksController.removeBook)
 
 
 module.exports = app
--- src/controller/index.js
+++ src/controller/index.js
@@ -28,13 +28,36 @@ async function post(req, res) {
         preco,
     })
 
-    book.save()
+    book.save(err => {
+        if(err) {
+            res.status(500).send({ message: `${err.message} - falha ao cadastrar o livro` })
+        } else {
+            res.status(201).json({ message: 'sucesso!' })
+        }
+    })
 
-    res.json({ message: 'sucesso!' })
+}
+
+async function updateBook(req, res) {
+    const { id } = req.params
+    const book = await BookModel.findByIdAndUpdate(id, req.body, { new: true })
+
+    res.send({ message: 'success', book })
+}
+
+async function removeBook(req, res) {
+    const { id } = req.params
+    const remove = await BookModel.deleteOne({ _id: id })
+
+    const message = remove.deletedCount ? 'success' : 'error'
+
+    res.send({ message })
 }
 
 module.exports = {
     getById,
     getAll,
     post,
+    updateBook,
+    removeBook,
 }
